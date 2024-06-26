fdf4195fbd45b1a92aa212692c770a379024c9fb
Matthias <web.bohn@gmail.com>
2024-06-26 11:33:45 +0200

    wifi esp

--- escapeRoomSketch/wifi.h
+++ escapeRoomSketch/wifi.h
@@ -5,10 +5,13 @@
 ################################################################ */
 #include <ESP8266WiFi.h>
 
-const char* ssid = "MrRobot";      // WIFI SSID for station mode
-const char* psk = "Pa55wortBohn";  // WIFI PSK
+const char* ssid = "EscapeRoomWifi";      // WIFI SSID for station mode
+const char* psk = "Pa55EscapeRoomWifiwortBohn";  // WIFI PSK
+
+//const char* ssid = "MrRobot";      // WIFI SSID for station mode
+//const char* psk = "Pa55wortBohn";  // WIFI PSK
 
 const char* ap_ssid = "EscapeRoomWifi";  // WIFI SSID for AP mode
 const char* ap_psk = "EscapeRoomWifi";   // WIFI PSK
 
-#define AP_MODE                     // comment for station mode
+//#define AP_MODE                     // comment for station mode
